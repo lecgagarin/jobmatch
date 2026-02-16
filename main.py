9eca66bbb3c49a50a38190733a9eeda67896b263
lecgagarin <dzidowski@gmail.com>
2026-02-16 15:19:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"status": "Job Matcher API is running 🚀"}
+
+@app.post("/upload-cv")
+async def upload_cv(file: UploadFile = File(...)):
+    
+    if not file.filename.endswith(".pdf"):
+        return JSONResponse(
+            status_code=400,
+            content={"error": "Only PDF files are allowed"}
+        )
+
+    contents = await file.read()
+
+    return {
+        "message": "CV uploaded successfully",
+        "filename": file.filename,
+        "size_bytes": len(contents)
+    }
